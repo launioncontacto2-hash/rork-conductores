c7cff518f84dbbb702c47e0d88322f9318a761f1
launioncontacto2-hash <launion.contacto2@gmail.com>
2026-09-23 18:59:48 -0600

    fix(uber-test): include auth events in Xcode target

--- uber-test/Sources/UberTestCore/UberTestBatchClient.swift
+++ uber-test/Sources/UberTestCore/UberTestBatchClient.swift
@@ -1,5 +1,18 @@
 import Foundation
 
+public extension Notification.Name {
+    static let uberTestSessionTerminated = Notification.Name("UberTest.sessionTerminated")
+}
+
+public enum UberTestSessionError: Error, Equatable {
+    case terminated
+}
+
+@inline(__always)
+public func uberTestNotifySessionTerminated() {
+    NotificationCenter.default.post(name: .uberTestSessionTerminated, object: nil)
+}
+
 public struct UberTestBatchClient: Sendable {
     public let functionURL: URL
     public let accessToken: @Sendable () async -> String?
